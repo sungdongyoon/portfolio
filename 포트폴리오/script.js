953173a50415a044a17825bd8fbd819329fcf8e6
aksen123 <ehdtjd0906@naver.com>
2023-04-18 19:56:25 +0900

    4/18 20:00

--- 포트폴리오/script.js
+++ 포트폴리오/script.js
@@ -12,26 +12,114 @@ $(function() {
 });
 
 /* Project slider */
+/* ===== slider one ===== */
 const projectOne = document.querySelector(".project_one_left");
-const images = ["반응형1-1.png", "반응형1-2.png", "반응형1-3.png", "반응형1-4.png", "반응형1-5.png", "반응형1-6.png"];
-const arrows = document.querySelectorAll(".arrow_one");
+const projectOneImages = ["반응형1-1.png", "반응형1-2.png", "반응형1-3.png", "반응형1-4.png", "반응형1-5.png", "반응형1-6.png"];
+const projectOneArrows = document.querySelectorAll(".arrow_one");
 
-projectOne.style.backgroundImage = `url(/img/${images[0]})`;
+projectOne.style.backgroundImage = `url(/img/${projectOneImages[0]})`;
 
 let i = 0;
-arrows.forEach(function(e) {
+projectOneArrows.forEach(function(e) {
   e.addEventListener("click", (e) => {
     if(e.target.id === "left_one") {
       i--;
       if(i < 0) {
-        i = images.length - 1;
+        i = projectOneImages.length - 1;
       }
     } else if(e.target.id === "right_one") {
       i++;
-      if(i >= images.length) {
+      if(i >= projectOneImages.length) {
         i = 0;
       }
     }
-    projectOne.style.backgroundImage = `url(/img/${images[i]})`;
+    projectOne.style.backgroundImage = `url(/img/${projectOneImages[i]})`;
   });
 });
+
+/* ===== slider two ===== */
+const projectTwo = document.querySelector(".project_two_right");
+const projectTwoImages = ["반응형2-1.png", "반응형2-2.png", "반응형2-3.png"];
+const projectTwoArrows = document.querySelectorAll(".arrow_two");
+
+projectTwo.style.backgroundImage = `url(/img/${projectTwoImages[0]})`;
+
+let j = 0;
+projectTwoArrows.forEach(function(e) {
+  e.addEventListener("click", (e) => {
+    if(e.target.id === "left_two") {
+      j--;
+      if(j < 0) {
+        j = projectTwoImages.length - 1;
+      }
+    } else if(e.target.id === "right_two") {
+      j++;
+      if(j >= projectTwoImages.length) {
+        j = 0;
+      }
+    }
+    projectTwo.style.backgroundImage = `url(/img/${projectTwoImages[j]})`;
+  });
+});
+
+/* ===== slider three ===== */
+const projectThree = document.querySelector(".project_three_left");
+const projectThreeImages = ["모바일1.png", "모바일2.png", "모바일3.png", "모바일4.png"];
+const projectThreeArrows = document.querySelectorAll(".arrow_three");
+
+projectThree.style.backgroundImage = `url(/img/${projectThreeImages[0]})`;
+
+let k = 0;
+projectThreeArrows.forEach(function(e) {
+  e.addEventListener("click", (e) => {
+    if(e.target.id === "left_three") {
+      k--;
+      if(k < 0) {
+        k = projectThreeImages.length - 1;
+      }
+    } else if(e.target.id === "right_three") {
+      k++;
+      if(k >= projectThreeImages.length) {
+        k = 0;
+      }
+    }
+    projectThree.style.backgroundImage = `url(/img/${projectThreeImages[k]})`;
+  });
+});
+
+/* Modal */
+/* ===== modal one ===== */
+const firstModal = document.querySelector(".project_one_modal");
+const firstBtn = document.querySelector(".pj_one_btn");
+const firstClose = document.querySelector(".close_btn_one");
+
+firstBtn.addEventListener("click", () => {
+  firstModal.classList.add("modal_active");
+})
+firstClose.addEventListener("click", () => {
+  firstModal.classList.remove("modal_active");
+})
+
+/* ===== modal two ===== */
+const secondModal = document.querySelector(".project_two_modal");
+const secondBtn = document.querySelector(".pj_two_btn");
+const secondClose = document.querySelector(".close_btn_two");
+
+secondBtn.addEventListener("click", () => {
+  secondModal.classList.add("modal_active");
+})
+secondClose.addEventListener("click", () => {
+  secondModal.classList.remove("modal_active");
+})
+
+/* ===== modal three ===== */
+const thirdModal = document.querySelector(".project_three_modal");
+const thirdBtn = document.querySelector(".pj_three_btn");
+const thirdClose = document.querySelector(".close_btn_three");
+
+thirdBtn.addEventListener("click", () => {
+  thirdModal.classList.add("modal_active");
+})
+thirdClose.addEventListener("click", () => {
+  thirdModal.classList.remove("modal_active");
+})
